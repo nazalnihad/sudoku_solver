8ee016c7760aa5ae518c46e5a552920e79691350
Nazal-nihad <nazalnihad17@gmail.com>
2023-11-05 03:28:13 +0530

    algorithm fixed minor edits to do

--- script.js
+++ script.js
@@ -26,11 +26,15 @@ function populateSudokuGrid() {
 
             cell.addEventListener('change', () => {
                 const value = parseInt(cell.value);
-                if (value > 9) {
+                if (value < 1 || value > 9) {
                     alert("Please enter a number from 1 to 9.");
-                    cell.value = 9; // Limit the input to 9
+                    cell.value = ''; // Clear the input
+                } else if (!cellCheck(row, col, value)) {
+                    alert("This number violates Sudoku rules.");
+                    cell.value = ''; // Clear the input
+                } else {
+                    enteredValues[row][col] = value;
                 }
-                enteredValues[row][col] = parseInt(cell.value) || 0;
             });
 
             sudoku.appendChild(cell);
@@ -38,8 +42,83 @@ function populateSudokuGrid() {
     }
 }
 
+
+function cellCheck(row, col, val) {
+    for (let i = 0; i < 9; i++) {
+        if (enteredValues[row][i] === val) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < 9; i++) {
+        if (enteredValues[i][col] === val) {
+            return false;
+        }
+    }
+
+    let r = Math.floor(row / 3) * 3;
+    let c = Math.floor(col / 3) * 3;
+    for (let i = r; i < r + 3; i++) {
+        for (let j = c; j < c + 3; j++) {
+            if (i >= 0 && i < 9 && j >= 0 && j < 9 && enteredValues[i][j] === val) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+function solve() {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (i >= 0 && i < 9 && j >= 0 && j < 9 && enteredValues[i][j] === 0) {
+                for (let val = 1; val <= 9; val++) {
+                    if (cellCheck(i, j, val)) {
+                        enteredValues[i][j] = val;
+                        if (solve()) {
+                            return true;
+                        }
+                        enteredValues[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true; // Indicates that the puzzle is solved
+}
+
+
+
+function isSudokuSolved() {
+    // Check if the Sudoku grid is completely filled
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (enteredValues[i][j] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+populateSudokuGrid();
 doneButton.addEventListener('click', () => {
+    // enteredValues[3][3] = 5;
+    solve();
+    populateSudokuGridWithValues();
     console.log(enteredValues);
 });
 
-populateSudokuGrid();
+
+function populateSudokuGridWithValues() {
+    const cells = document.getElementsByClassName('sudoku-cell');
+    let index = 0;
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            cells[index].value = enteredValues[row][col].toString();
+            index++;
+        }
+    }
+}
